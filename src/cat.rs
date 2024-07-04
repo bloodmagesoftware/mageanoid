f20045117f2f6d4af4c2337d5d2dfdc90093abd3
Frank Mayer <gh@frankmayer.dev>
2024-07-04 14:32:33 +0200

    cat not spawning behund mage

--- src/cat.rs
+++ src/cat.rs
@@ -49,8 +49,12 @@ fn spawn_cat(
                 layout: texture_atlas_layout,
                 index: animation_indices.first,
             },
-            transform: Transform::from_scale(Vec3::splat(2.0)),
-            ..Default::default()
+            transform: Transform::from_scale(Vec3::splat(2.0)).with_translation(Vec3::new(
+                CAT_THRESHOLD,
+                CAT_THRESHOLD,
+                0.0,
+            )),
+            ..default()
         },
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.25, TimerMode::Repeating)),
